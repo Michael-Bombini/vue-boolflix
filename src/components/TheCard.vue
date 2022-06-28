bc3d2281f8efdaf234404faa2737c7b4d73ab45a
Michael Bombini <bombini.michael@gmail.com>
2022-06-28 21:24:01 +0200

    Fixed correctly stars visualization

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -12,9 +12,20 @@
         <h6 class="card-title">Lingua : {{ card.original_language }}</h6>
         <p class="card-text">Descrizione.....</p>
         <p>Voto {{ printVoto(card.vote_average) }}</p>
-        <span v-for="n in printVoto(card.vote_average)" :key="n"
-          ><img src="https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/star.png" alt="" class="star"></span
-        >
+        <span v-for="n in 5" :key="n">
+          <img
+            src="https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/star.png"
+            alt=""
+            class="star"
+            v-if="n <= printVoto(card.vote_average)"
+          />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Five_Pointed_Star_Solid.svg/2175px-Five_Pointed_Star_Solid.svg.png"
+            class="star"
+            alt=""
+            v-else
+          />
+        </span>
       </div>
     </div>
   </div>
